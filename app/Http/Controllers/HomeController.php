27bae643aa2c1abf1495511e9f95298e2071537b
menghieng-sorn <menghiengsorn.it@gmail.com>
2026-07-25 15:27:29 +0700

    update v44

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Product;
-use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
@@ -15,9 +12,28 @@ class HomeController extends Controller
         //$users = DB::table('users')->get();
 
         //fetch Single Data
-        $users = DB::table('users')->where('id',1)->first();
+        // $users = DB::table('users')->where('id',1)->first();
 
-        dd($users);
+        //Select Culomn
+        //$users = DB::table('blogs')->select('title')->get();
+
+        //Colletion
+        //$users = DB::table('blogs')->pluck('title');
+        // $users = DB::table('blogs')->pluck('title','id')->toArray();
+
+        //get all product
+        //$products = DB::table('products')->get();
+
+        //Count Product
+        //$products = DB::table('products')->count();
+
+        //get max/min price
+        //$products = DB::table('products')->min('price');
+
+        //sum/avg price
+        $products = DB::table('products')->avg('price');
+
+        dd($products);
         return view('home');
     }
     function store()
@@ -30,6 +46,17 @@ class HomeController extends Controller
             ]
         );
         return view('home');
+    }
+    function update(){
+        DB::table('users')->where('id',1)->update(
+            [
+                'name'=>'Jam002',
+            ]
+        );
+    }
+    function delete()
+    {
+        DB::table('users')->where('id',1)->delete();
     }
     function showAboutPage()
     {
